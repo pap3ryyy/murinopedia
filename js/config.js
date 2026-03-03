1dcf592a609268657e8e2378e7aa0f6fe21562c6
pap3ryyy <vertexiousdev@gmail.com>
2026-03-03 09:12:37 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,3 @@
+const SB_URL = "https://rvykoymrumzgkgbwdefm.supabase.co";
+const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2eWtveW1ydW16Z2tnYndkZWZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyMTcxMjksImV4cCI6MjA4Njc5MzEyOX0.4vBw0N_f8Xtr7P8HQd4tPG90m2fuulRGAqRO-K3lZ-M";
+const supabaseClient = supabase.createClient(SB_URL, SB_KEY);
