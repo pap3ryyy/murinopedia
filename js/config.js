baed35a277f34e781b689b2ecce6ea684be7af7f
pap3ryyy <vertexiousdev@gmail.com>
2026-05-04 15:34:57 +0300

    Update Supabase URL and key in config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const SB_URL = "https://rvykoymrumzgkgbwdefm.supabase.co";
-const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2eWtveW1ydW16Z2tnYndkZWZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyMTcxMjksImV4cCI6MjA4Njc5MzEyOX0.4vBw0N_f8Xtr7P8HQd4tPG90m2fuulRGAqRO-K3lZ-M";
+const SB_URL = "https://rgvqhnttzabwbuvsvgdx.supabase.co";
+const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJndnFobnR0emFid2J1dnN2Z2R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4OTE1MDksImV4cCI6MjA5MzQ2NzUwOX0.yBL0n2CI6fmz-NUT0VBc3tRN6OqYReBjtKgFQFQWMT0";
 const supabaseClient = supabase.createClient(SB_URL, SB_KEY);
